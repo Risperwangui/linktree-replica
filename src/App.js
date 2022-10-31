28b9511b71ace6d71ec30929a26804a362c0142b
Risperwangui <99345570+Risperwangui@users.noreply.github.com>
2022-10-31 16:42:25 +0300

    Added a link section with respective links having tooltips for their descriptions.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Tooltip, Box } from '@mantine/core';
  
 
 function App() {
@@ -23,28 +24,73 @@ function App() {
     // </div>
 
    
-    <div className="container">
-      <div  className="main_section">
-        <div className="content">
-          <div id="profile-section">
-            <img src="profile_picture2.jpeg" alt="profile" id="profile__img"></img>
-            <div className="twitter">
-              <p id="twitter"> Wangui Resper</p>
-            </div>
-            <p id="slack"> Risper Wangui</p>
-            <div className="avatar">
-              <div className="avatar_content">
-                <div className="share">
-                  <svg id="share_svg" width="14" height="12" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M16.3261 8.50617C16.5296 8.3318 16.6313 8.24461 16.6686 8.14086C16.7013 8.0498 16.7013 7.9502 16.6686 7.85914C16.6313 7.75539 16.5296 7.6682 16.3261 7.49383L9.26719 1.44331C8.917 1.14315 8.74191 0.993063 8.59367 0.989386C8.46483 0.986191 8.34177 1.04279 8.26035 1.14269C8.16667 1.25764 8.16667 1.48825 8.16667 1.94948V5.52886C6.38777 5.84007 4.75966 6.74146 3.54976 8.09489C2.23069 9.57043 1.50103 11.48 1.5 13.4591V13.9691C2.37445 12.9157 3.46626 12.0638 4.70063 11.4716C5.78891 10.9495 6.96535 10.6403 8.16667 10.5588V14.0505C8.16667 14.5117 8.16667 14.7424 8.26035 14.8573C8.34177 14.9572 8.46483 15.0138 8.59367 15.0106C8.74191 15.0069 8.917 14.8569 9.26719 14.5567L16.3261 8.50617Z" stroke="#98A2B3" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-                  </svg>
+    <div class="main">
+      <div className="container">
+        <div  className="main_section">
+          <div className="content">
+            <div id="profile-section">
+              <img src="profile_picture2.jpeg" alt="profile" id="profile__img"></img>
+              <div className="twitter">
+                <p id="twitter"> Wangui Resper</p>
+              </div>
+              <p id="slack"> Risper Ndungu </p>
+              <div className="avatar">
+                <div className="avatar_content">
+                  <div className="share">
+                    <img id='share_svg' src='share.svg'></img>
+                  </div>
                 </div>
+              </div>
+            </div>
+            <div className="Link-section">
+              <div className="links">
+                <Tooltip.Floating label="My twitter handle" color="gray">
+                  <div className="link-frame">
+                    <a target="_blank" href="https://www.twitter.com/WanguiResper" id="btn__twitter">Twitter link</a>
+                  </div>
+                </Tooltip.Floating>
+              </div>
+              <div className="links">
+                <Tooltip.Floating label="Welcome to Zuri team" color="gray">
+                  <div className="link-frame">
+                    <a target="_blank" href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a>
+                  </div>
+                </Tooltip.Floating>
+              </div>
+              <div className="links">
+                <Tooltip.Floating label="This where to find boooks on design and coding" color="gray">
+                  <div className="link-frame">
+                    <a target="_blank" href="http://books.zuri.team" id="books">Zuri Books</a>
+                  </div>
+                </Tooltip.Floating>
+              </div>
+              <div className="links">
+                <Tooltip.Floating multiline width={400} label="If you're thinking of embarking on your journey in Python,this is the book for you!You cannot afford to miss this!Grab yours today!" color="gray">
+                  <div className="link-frame">
+                    <a target="_blank" href="https://books.zuri.team/python-for-beginners?ref_id=<Risper Ndungu>" id="book__python">Python Books</a>
+                  </div>
+                </Tooltip.Floating>
+              </div>
+              <div className="links">
+                <Tooltip.Floating  multiline width={300} label="Need background checks done on your coders?Here is the place for you! " color="gray">
+                  <div className="link-frame">
+                    <a target="_blank" href="https://background.zuri.team " id="pitch">Background Check for coders</a>
+                  </div>
+                </Tooltip.Floating>
+              </div>
+              <div className="links">
+                <Tooltip.Floating multiline width={400} label="Tested and Proven! This is hands on the best design book you could ever find! And what's even better? Here at Zuri we offer it for free!Grab yours today!" color="gray">
+                  <div className="link-frame">
+                    <a target="_blank" href="https://books.zuri.team/design-rules " id="book__design">Design books</a>
+                  </div>
+                </Tooltip.Floating>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    
   )
 }
 
