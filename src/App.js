d663dde1d72a6f2d734192e5d84a27cbefe2ad11
Risperwangui <99345570+Risperwangui@users.noreply.github.com>
2022-11-01 12:26:55 +0300

    Updated links to make the frames clickable.

--- src/App.js
+++ src/App.js
@@ -48,44 +48,33 @@ function App() {
             <div className="Link-section">
               <div className="links">
                 <Tooltip.Floating label="My twitter handle" color="gray">
-                  <div className="link-frame">
-                    <a target="_blank" href="https://www.twitter.com/WanguiResper" id="btn__twitter">Twitter link</a>
-                  </div>
+                    <a target="_blank" href="https://www.twitter.com/WanguiResper" id="btn__twitter"><span className="btn-twitter">Twitter link</span></a>
+                  
                 </Tooltip.Floating>
               </div>
               <div className="links">
                 <Tooltip.Floating label="Welcome to Zuri team" color="gray">
-                  <div className="link-frame">
-                    <a target="_blank" href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a>
-                  </div>
+                    <a target="_blank" href="https://training.zuri.team/" id="btn__zuri"><span class="btn-zuri">Zuri Team</span></a>
                 </Tooltip.Floating>
               </div>
               <div className="links">
                 <Tooltip.Floating label="This where to find boooks on design and coding" color="gray">
-                  <div className="link-frame">
-                    <a target="_blank" href="http://books.zuri.team" id="books">Zuri Books</a>
-                  </div>
+                    <a target="_blank" href="http://books.zuri.team" id="books"><span class="btn-books">Zuri Books</span></a>
                 </Tooltip.Floating>
               </div>
               <div className="links">
                 <Tooltip.Floating multiline width={400} label="If you're thinking of embarking on your journey in Python,this is the book for you!You cannot afford to miss this!Grab yours today!" color="gray">
-                  <div className="link-frame">
-                    <a target="_blank" href="https://books.zuri.team/python-for-beginners?ref_id=<Risper Ndungu>" id="book__python">Python Books</a>
-                  </div>
+                    <a target="_blank" href="https://books.zuri.team/python-for-beginners?ref_id=<Risper Ndungu>" id="book__python"><span class="btn-python">Python Books</span></a>
                 </Tooltip.Floating>
               </div>
               <div className="links">
                 <Tooltip.Floating  multiline width={300} label="Need background checks done on your coders?Here is the place for you! " color="gray">
-                  <div className="link-frame">
-                    <a target="_blank" href="https://background.zuri.team " id="pitch">Background Check for coders</a>
-                  </div>
+                    <a target="_blank" href="https://background.zuri.team " id="pitch"><span class="btn-pitch">Background Check for coders</span></a>
                 </Tooltip.Floating>
               </div>
               <div className="links">
                 <Tooltip.Floating multiline width={400} label="Tested and Proven! This is hands on the best design book you could ever find! And what's even better? Here at Zuri we offer it for free!Grab yours today!" color="gray">
-                  <div className="link-frame">
-                    <a target="_blank" href="https://books.zuri.team/design-rules " id="book__design">Design books</a>
-                  </div>
+                    <a target="_blank" href="https://books.zuri.team/design-rules " id="book__design"><span class="btn-design">Design books</span></a>
                 </Tooltip.Floating>
               </div>
             </div>
